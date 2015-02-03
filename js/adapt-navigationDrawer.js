b58132718203b207999a6d5a109c2f52cb78ae7b
Jerome Lam <jerome.lam@gmail.com>
2015-02-03 18:22:40 -0500

    Add basic structure (view and styles).

--- js/adapt-navigationDrawer.js
+++ js/adapt-navigationDrawer.js
@@ -1,18 +1,17 @@
 /*
-* adapt-navigationDrawer
-* License - https://github.com/JeromeLam/adapt-navigationDrawer/blob/master/LICENSE
-* Maintainers - Jerome Lam <jerome.lam@gmail.com>
-*/
+ * adapt-navigationDrawer
+ * License - https://github.com/JeromeLam/adapt-navigationDrawer/blob/master/LICENSE
+ * Maintainers - Jerome Lam <jerome.lam@gmail.com>
+ */
 
 define(function(require) {
 
     var Adapt = require('coreJS/adapt');
     var Backbone = require('backbone');
-    var ExtensionView = require('extensions/adapt-extension/js/adapt-extensionView');
 
-    // Listen to when the data is all loaded
-    Adapt.on('app:dataReady', function() {
-        console.log('Plugin has loaded and data is ready');
-    });
+    var navigationDrawer = new(Backbone.View.extend({}))();
 
+    navigationDrawer.$el = $('<div>').addClass("navigationDrawer").appendTo($("body"));
+
+    Adapt.navigationDrawer = navigationDrawer;
 });
