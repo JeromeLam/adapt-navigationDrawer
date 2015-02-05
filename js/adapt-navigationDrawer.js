b31ba251b0ad9ba4a3dd51adce3556756c62700b
Jerome Lam <jerome.lam@gmail.com>
2015-02-05 18:29:38 -0500

    Load navigationDrawerView into navigationDrawer and add event handlers for opening/closing navigationDrawer.

--- js/adapt-navigationDrawer.js
+++ js/adapt-navigationDrawer.js
@@ -8,10 +8,48 @@ define(function(require) {
 
     var Adapt = require('coreJS/adapt');
     var Backbone = require('backbone');
+    var NavigationDrawerView = require('extensions/adapt-navigationDrawer/js/adapt-navigationDrawerView');
 
-    var navigationDrawer = new(Backbone.View.extend({}))();
+    var navigationDrawer = new (Backbone.View.extend({
 
-    navigationDrawer.$el = $('<div>').addClass("navigationDrawer").appendTo($("body"));
+        initialize: function() {
+            this.listenTo(Adapt, 'navigationDrawer:open', this.show);
+            this.listenTo(Adapt, 'navigationDrawer:close', this.hide);
+            this.preRender();
+            this.render();
+        },
+
+        preRender: function() {},
+
+        render: function() {
+            this.$el = $('<div>').addClass("navigationDrawer").appendTo($("body"));
+
+            var template = Handlebars.templates["navigationDrawer"];
+            this.$el.html(template());
+
+            _.defer(_.bind(function() {
+                this.postRender();
+            }, this));
+        },
+
+        postRender: function() {
+            var $navigationDrawerInner = this.$('.navigationDrawer-inner').empty();
+
+            var navigationDrawerView = new NavigationDrawerView({model: new Backbone.Model({})});
+            this.$('.navigationDrawer-inner').html(navigationDrawerView.$el.html());
+        },
+
+        show: function() {
+            this.$el.addClass('active');
+        },
+        hide: function() {
+            this.$el.removeClass('active');
+        }
+    }))();
+
+    Adapt.once("app:dataReady", function() {
+        navigationDrawer.model = new Backbone.Model(Adapt.course.get("_navigationDrawer"));
+    });
 
     Adapt.navigationDrawer = navigationDrawer;
 });
