a5e057ceb6d2169c19beade543b87e07fa69d9e2
Jerome Lam <jerome.lam@gmail.com>
2015-02-05 18:27:52 -0500

    Add the navigationDrawerItem template and link it to navigationDrawerView.

--- js/adapt-navigationDrawerView.js
+++ js/adapt-navigationDrawerView.js
@@ -1,24 +1,20 @@
 /*
-* adapt-navigationDrawer
-* License - https://github.com/JeromeLam/adapt-navigationDrawer/blob/master/LICENSE
-* Maintainers - Jerome Lam <jerome.lam@gmail.com>
-*/
+ * adapt-navigationDrawer
+ * License - https://github.com/JeromeLam/adapt-navigationDrawer/blob/master/LICENSE
+ * Maintainers - Jerome Lam <jerome.lam@gmail.com>
+ */
 
 define(function(require) {
 
     var Adapt = require('coreJS/adapt');
     var Backbone = require('backbone');
 
-    var ExtensionView = Backbone.View.extend({
+    var NavigationDrawerView = Backbone.View.extend({
 
-        className: "extension",
+        className: "navigationDrawerView",
 
         initialize: function () {
-            // Listen to Adapt 'remove' event which is called
-            // when navigating through the router
-            // This cleans up zombie views and prevents memory leaks
             this.listenTo(Adapt, 'remove', this.remove);
-            // On initialize start the render process
             this.preRender();
             this.render();
         },
@@ -28,31 +24,20 @@ define(function(require) {
         preRender: function() {},
 
         render: function () {
-            // Convert model data into JSON
             var data = this.model.toJSON();
-            // Get handlebars template
-            var template = Handlebars.templates["extension"];
-            // Push data into template and append template
-            this.$el.html(template(data)).appendTo('#wrapper');
-            // Defer is used here to make sure the template has rendered
-            // before calling postRender
-            // This way postRender can manipulate this view after it has
-            // been rendered
+            var template = Handlebars.templates["navigationDrawerItem"];
+            this.$el.html(template(data));
             _.defer(_.bind(function() {
                 this.postRender();
             }, this));
-
-            // Return this so we can change the render method
-            // this.render().$el.addClass('explode');
             return this;
 
         },
 
         postRender: function() {}
-
+s
     });
-    
-    // Return ExtensionView so it can be required
-    return ExtensionView;
+
+    return NavigationDrawerView;
 
 });
